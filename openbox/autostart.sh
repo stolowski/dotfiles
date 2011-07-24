96ef5aa8efd892ad5d583382f6da0a646d967205
Pawel Stolowski <pawel@pawel-mini.(none)>
2011-07-24 08:56:04 +0100

    Added Openbox autostart.

---
+++ openbox/autostart.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+nitrogen --restore &
+avant-window-navigator &
+wicd-gtk &
+gnote &
+#nm-applet &
+thunar --daemon &
+~/.dropbox-dist/dropboxd &
+xscreensaver -no-splash &
+xfce4-power-manager &
+update-notifier &
