74a4936b2a54ffe7e4d222db788f1950641f8a95
Pawel Stolowski <stolowski@gmail.com>
2011-10-16 21:04:31 +0100

    Openbox - updated autostart.

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 nitrogen --restore &
-avant-window-navigator &
+pulseaudio --start &
+#avant-window-navigator &
+tint2 &
 wicd-gtk &
 gnote &
 #nm-applet &
 thunar --daemon &
 ~/.dropbox-dist/dropboxd &
 xscreensaver -no-splash &
-xfce4-power-manager &
+#xfce4-power-manager &
+parcellite &
 update-notifier &
+unclutter &
